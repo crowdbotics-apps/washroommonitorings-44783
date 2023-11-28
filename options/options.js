8fac62f29e2566dde45b4d6523085f387e45e092
Crowdbotics <team@crowdbotics.com>
2023-11-28 16:44:07 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "washroommonitorings-44783",
+  url: "https://washroommonitorings-44783.botics.co",
+  api: "https://washroommonitorings-44783.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
